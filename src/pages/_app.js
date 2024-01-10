a38439ca6c6648095312719089f1134a1fb4abcd
Nelson <tomneo2004@gmail.com>
2024-01-10 12:14:18 +0800

    project setup and style

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Roboto_Mono } from "next/font/google";
+
+const robot_mono = Roboto_Mono({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={robot_mono.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
